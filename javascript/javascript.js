ec8a85c8708803b538fda751d2cf215e8857095e
Michael Johansyah <johansyahmichael@gmail.com>
2025-03-08 22:43:56 +0700

    just adding some random things

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,30 @@
-const lotsOfDecimal = 1.766584958675746364;
-console.log(lotsOfDecimal);
-const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
-console.log(twoDecimalPlaces);
+// const myText = "I am a string";
+// function changeString(newString){
+//     return myText.replace("string", newString);
+// }
+
+// console.log(changeString('human'));
+
+
+
+// const lotsOfDecimal = 1.766584958675746364;
+// console.log(lotsOfDecimal);
+// const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+// console.log(twoDecimalPlaces);
+
+// function favoriteAnimal(animal) {
+//     return animal + " is my favorite animal!"
+// }
+
+// console.log(favoriteAnimal('Goat'))
+
+// const myArray = ["I", "love", "chocolate", "frogs"];
+// const madeAString = myArray.join(" ");
+// console.log(madeAString);
+
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
+
+textBox.addEventListener("keydown", (event) => {
+    output.textContent = `You pressed "${event.key}".`;
+});
