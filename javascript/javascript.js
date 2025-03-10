481acb0ad30d087b8f269cd0cb3430c2d38d9ca1
Michael Johansyah <johansyahmichael@gmail.com>
2025-03-10 17:53:50 +0700

    Complete assignment on Function Basics

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,5 @@
 // const myText = "I am a string";
+
 // function changeString(newString){
 //     return myText.replace("string", newString);
 // }
@@ -22,9 +23,64 @@
 // const madeAString = myArray.join(" ");
 // console.log(madeAString);
 
-const textBox = document.querySelector("#textBox");
-const output = document.querySelector("#output");
+// const textBox = document.querySelector("#textBox");
+// const output = document.querySelector("#output");
 
-textBox.addEventListener("keydown", (event) => {
-    output.textContent = `You pressed "${event.key}".`;
-});
+// textBox.addEventListener("keydown", (event) => {
+//     output.textContent = `You pressed "${event.key}".`;
+// });
+
+// function min(a,b){
+//     if (a < b){
+//         return a;
+//     }
+//     else if (a > b ){
+//         return b;
+//     }
+//     else{
+//         return a;
+//     }
+// }
+
+// alert(min(3,3));
+
+// function pow(a,b){
+//     return a**b;
+// }
+
+// alert(pow(2,7));
+
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+  
+//   function showOk() {
+//     alert( "You agreed." );
+//   }
+  
+//   function showCancel() {
+//     alert( "You canceled the execution." );
+//   }
+  
+//   // usage: functions showOk, showCancel are passed as arguments to ask
+//   ask("Do you agree?", showOk, showCancel);
+
+function add7(a){
+  return a + 7;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+
+function capitalize(string){
+  return string[0].toUpperCase();
+}
+
+function lastLetter(string){
+  return string[(string.length) - 1];
+}
+
+console.log(capitalize("striung"));
+console.log(lastLetter("abcd"));
