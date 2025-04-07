0c0c0c815386a6fb97e1e972ef9dbfc1d3edf314
Michael Johansyah <johansyahmichael@gmail.com>
2025-04-07 14:32:38 +0700

    Doing JS Array Method Tasks

---
+++ javascript/arrayMethod.js
@@ -0,0 +1,18 @@
+// 1. Translate border-left-width to borderLeftWidth
+
+function camelize(text){
+    return text.split('-').map;
+}
+console.log(camelize("background-color"));
+
+// 2. Filter range
+// 3. Filter range "in place"
+// 4. Sort in decreasing order
+// 5. Copy and sort array
+// 6. Map to names
+// 7. Map to objects
+// 8. Sort users by age
+// 9. Shuffle an array
+// 10. Get average age
+// 11. Filter unique array members
+// 12. Create keyed object from array
--- javascript/js4.js
+++ javascript/js4.js
@@ -1,40 +1,64 @@
-// let menu = {
-//     width: 200,
-//     height: 300,
-//     title: "My menu",
-// };
-// function multiplyNumeric(menu) {
-//     for (key in menu) {
-//         if (typeof key === "number") {
-//             menu[key] *= 2;
-//         }
+// // let menu = {
+
+// //     width: 200,
+// //     height: 300,
+// //     title: "My menu",
+// // };
+// // function multiplyNumeric(menu) {
+// //     for (key in menu) {
+// //         if (typeof key === "number") {
+// //             menu[key] *= 2;
+// //         }
+// //     }
+// // }
+
+// // multiplyNumeric(menu);
+
+// // alert(menu.height);
+// // alert(menu.width);
+// // alert(menu.title);
+
+// // const numbers = [1, 4, 7, 2, 9, 6, 3];
+
+// // const evenSum = numbers.reduce((acc, num) => {
+// //     if(num % 2 === 0){
+// //         acc += num;
+// //     }
+// //     return acc;
+// //   }, 0);
+
+// // console.log(numbers);
+// // console.log(evenSum);
+// const words = ["apple", "banana", "kiwi", "strawberry", "mango"];
+
+// const longestWord = words.reduce((longest, currentWord) => {
+//     if (currentWord.length > longest.length){
+//         longest = currentWord;
 //     }
+//     return longest;
+//   }, "");
+
+// console.log(longestWord);
+
+// 
+
+// let arr = ["HTML", "JavaScript", "CSS"];
+// let sorted = copySorted(arr);
+
+// function copySorted(arr){
+//   return arr.slice().sort();
 // }
 
-// multiplyNumeric(menu);
+// console.log( sorted ); // CSS, HTML, JavaScript
+// console.log( arr ); // HTML, JavaScript, CSS (no changes)
 
-// alert(menu.height);
-// alert(menu.width);
-// alert(menu.title);
 
-// const numbers = [1, 4, 7, 2, 9, 6, 3];
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
 
-// const evenSum = numbers.reduce((acc, num) => {
-//     if(num % 2 === 0){
-//         acc += num;
-//     }
-//     return acc;
-//   }, 0);
+let users = [ john, pete, mary ];
 
-// console.log(numbers);
-// console.log(evenSum);
-const words = ["apple", "banana", "kiwi", "strawberry", "mango"];
+let names = users.map(names => ' '+ names.name);
 
-const longestWord = words.reduce((longest, currentWord) => {
-    if (currentWord.length > longest.length){
-        longest = currentWord;
-    }
-    return longest;
-  }, "");
-
-console.log(longestWord);
+alert( names ); // John, Pete, Mary
