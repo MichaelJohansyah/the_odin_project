46bb91a84d06d8b502162a248959aceaf1eb289d
Michael Johansyah <johansyahmichael@gmail.com>
2025-04-07 14:46:10 +0700

    Doing JS Array Method Tasks

--- javascript/arrayMethod.js
+++ javascript/arrayMethod.js
@@ -1,11 +1,26 @@
 // 1. Translate border-left-width to borderLeftWidth
-
-function camelize(text){
-    return text.split('-').map;
-}
-console.log(camelize("background-color"));
+// function camelize(text) {
+//     return text
+//         .split("-")
+//         .map((text, index) => {
+//             if (index == 0) {
+//                 return text;
+//             } else {
+//                 return text[0].toUpperCase() + text.slice(1);
+//             }
+//         })
+//         .join("");
+// }
+// console.log(camelize("background-color"));
 
 // 2. Filter range
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (matching values)
+
+alert( arr ); // 5,3,8,1 (not modified)
 // 3. Filter range "in place"
 // 4. Sort in decreasing order
 // 5. Copy and sort array
